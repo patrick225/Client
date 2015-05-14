f6f530822ca7f86ebdd88067997f2863173d0bac
Patrick Lutz <patrick.lutz1992@gmail.com>
2015-05-14 14:30:49 +0200

    ShotDetector implemented

---
+++ app/src/main/java/com/patricklutz/ba/client/OnMessageReceived.java
@@ -0,0 +1,9 @@
+package com.patricklutz.ba.client;
+
+/**
+ * Created by privat-patrick on 12.05.2015.
+ */
+public interface OnMessageReceived {
+
+    void messageReceived(String message);
+}
---
+++ app/src/main/java/com/patricklutz/ba/client/ShotDetector.java
@@ -0,0 +1,49 @@
+package com.patricklutz.ba.client;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+
+/**
+ * SensorEventListener to detect Shots
+ * Created by privat-patrick on 14.05.2015.
+ */
+public class ShotDetector implements SensorEventListener {
+
+    private static final int SHAKE_THRESHOLD = 2500;
+
+    long lastUpdate = 0;
+    double x = 0, y = 0, z = 0;
+    double last_x = 0, last_y = 0, last_z = 0;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long difftime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2];
+
+                double speed = Math.abs(x+y+z - last_x - last_y - last_z) / difftime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    Log.d("Sensor", "shake detected speed: " + speed);
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
